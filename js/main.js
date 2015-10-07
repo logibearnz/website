3fbc89a8cb5c36abd97d8599e1ff06c91e837029
Logan WIffen <logibearnz@gmail.com>
2015-10-07 14:41:47 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 function submitHandler(){
     var $submitButton = $('#submitButton');
         $submitButton.on('click', function() {
-            console.log('Submit');
+            //console.log('Submit');
 	    var return_to = getQueryParam('return_to', 'pebblejs://close#');
 	    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });

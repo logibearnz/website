25a2b305b2e70c13bc9d0aaeef9985ed05964fbc
Logan WIffen <logibearnz@gmail.com>
2015-10-10 09:42:39 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,66 @@
-var $submitButton = $('#submitButton');
-	$submitButton.on('click', function() {
-		console.log('Submit');
-		var return_to = getQueryParam('return_to', 'pebblejs://close#');
-		document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+(function() {
+    loadOptions();
+    submitHandler();
+})();
+
+function submitHandler(){
+    var $submitButton = $('#submitButton');
+        $submitButton.on('click', function() {
+            //console.log('Submit');
+	    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+	    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 });
+
+}
 
 function getAndStoreConfigData() {
 	var $backgroundColorPicker = $('#backgroundColorPicker');
-	var $seconds-toggle = $('#seconds-toggle');
-	var $date-toggle = $('#date-toggle');
+	var $secondstoggle = $('#secondstoggle');
+	var $datetoggle = $('#datetoggle');
+	var $dateformat = $('#dateformat');
 
 	var options = {
 		backgroundColor: $backgroundColorPicker.val(),
-		seconds-enabled: $seconds-toggle[0].checked
-		date-enabled: $date-toggle[0].checked
+		secondsenabled: $secondstoggle[0].checked,
+		dateenabled: $datetoggle[0].checked,
+		dateformat: $dateformat[0].checked
 	};
 
 	localStorage.backgroundColor = options.backgroundColor;
-	localStorage.seconds-enabled = options.seconds-enabled;
-	localStorage.date-enabled = options.date-enabled;
+	localStorage.secondsenabled = options.secondsenabled;
+	localStorage.dateenabled = options.dateenabled;
+	localStorage.dateformat = options.dateformat;
 
-	console.log('Got optionsL ' + JSON.stringify(options));
+	console.log(JSON.stringify(options));
 	return options;
+}
 
-
-
-
+function loadOptions(){
+    var $backgroundColorPicker = $('#backgroundColorPicker');
+    var $secondstoggle = $('#secondstoggle');
+    var $datetoggle = $('#datetoggle');
+    var $dateformat = $('#dateformat');
+    
+    if (localStorage.backgroundColor) {
+        $backgroundColorPicker[0].value = localStorage.backgroundColor;
+        $secondstoggle[0].checked = localStorage.secondsenabled; //=== 'true';
+	$datetoggle[0].checked = localStorage.dateenabled;// === 'true';
+	$dateformat[0].checked = localStorage.dateformat;// === 'true';
+    }
+}
 
 
 function getQueryParam(variable, defaultValue) {
-	var query = location.search.substring(1);
-	var vars = query.split('&');
-	for (var i = 0; i < vars.length: i++) {
-		var pair = vars[i].split('=');
-		if (pair[0] === variable){
-			return decodeURIComponent(pair[1});
-		}
-	}
-	return defaultValue || false;
+  // Find all URL parameters
+  var query = location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+
+    // If the query variable parameter is found, decode it to use and return it for use
+    if (pair[0] === variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return defaultValue || false;
 }

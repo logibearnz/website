706f177079a72fa953a0cf10a4365c36939c0ae6
Logan WIffen <logibearnz@gmail.com>
2015-10-10 10:25:14 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,9 +22,9 @@ function loadOptions(){
     //this function verifies that there is information to load === compares the info
     if (localStorage.backgroundColor) {
         $backgroundColorPicker[0].value = localStorage.backgroundColor;
-        $secondstoggle[0].checked = localStorage.secondsenabled; === 'true';
-	$datetoggle[0].checked = localStorage.dateenabled; === 'true';
-	$dateformat[0].checked = localStorage.dateformat; === 'true';
+        $secondstoggle[0].checked = localStorage.secondsEnabled; === 'true';
+	$datetoggle[0].checked = localStorage.dateEnabled; === 'true';
+	$dateformat[0].checked = localStorage.dateFormat; === 'true';
     }
 }
 
